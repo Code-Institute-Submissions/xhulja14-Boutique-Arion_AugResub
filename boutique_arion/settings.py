b51bef2ce54e8dfe451a3bff2fc75328e47561bc
xhulja14 <xhulja14@gmail.com>
2021-05-24 00:16:00 +0000

    deployment part 2

--- boutique_arion/settings.py
+++ boutique_arion/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)&l$_0ox(_3!y4rv928f2de3u$0rjx%jg22@$)fkyk9=rc!gp=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['xhulja14-boutique-arion.heroku.com', 'localhost']
 
 
 # Application definition
@@ -122,13 +122,18 @@ WSGI_APPLICATION = 'boutique_arion.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
