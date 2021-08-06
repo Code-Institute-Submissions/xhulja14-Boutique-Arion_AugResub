769ba9564dcb0872c4e7ab2b348c40b6f2544ed7
xhulja14 <xhulja14@gmail.com>
2021-08-06 15:31:54 +0000

    rename image links

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class BlogPost(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	title = models.CharField(max_length=200)
 	body_text = models.TextField(default=None, blank=True, null=True)
-	created_at = models.CharField(default=None, blank=True)
+	created_at = models.CharField(max_length=200)
 
 	def __str__(self):
 		return self.title + ' | ' + str(self.created_at)
@@ -17,5 +17,5 @@ class BlogPost(models.Model):
 class Comments(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	post = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
-	created_at = models.CharField(default=None, blank=True)
+	created_at = models.CharField(max_length=200)
 	comment = models.TextField(default=None, blank=True, null=True)
